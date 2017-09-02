082ec8a7850ff072b4113ee8b5d7517065d6d797
royyjzhang <royyjzhang@outlook.com>
2017-09-01 22:32:42 -0700

    319 Bulb Switcher

---
+++ 319_BulbSwitcher.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<stdlib.h>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<math.h>
+using namespace std;
+
+class Solution {
+public:
+	int bulbSwitch(int n) {
+		return sqrt(n);
+	}
+};
+
+int main() {
+	int n=3;
+	Solution v;	
+	cout << v.bulbSwitch(n) << endl;
+	system("PAUSE");
+	return 0;
+}
